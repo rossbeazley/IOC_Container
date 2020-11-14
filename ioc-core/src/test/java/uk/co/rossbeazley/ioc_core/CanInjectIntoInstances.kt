2556b42e2f1e26ce77ee595da2c4401921a40a67
beazlr02 <ross.beazley@bbc.co.uk>
2020-11-14 14:40:44 +0000

    Remove autogenerated examople tests

---
+++ ioc-core/src/test/java/uk/co/rossbeazley/ioc_core/CanInjectIntoInstances.kt
@@ -0,0 +1,12 @@
+package uk.co.rossbeazley.ioc_core
+
+import org.junit.Test
+
+class CanInjectIntoInstances {
+
+    @Test
+    fun theOneWhereAnInstanceAsDependenciesInjectedThroughSetters()
+    {
+
+    }
+}
