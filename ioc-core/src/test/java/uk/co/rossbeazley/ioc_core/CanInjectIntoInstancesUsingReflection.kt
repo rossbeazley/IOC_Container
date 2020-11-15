64fee90a154b4911386c24ea353084d8a92115d4
beazlr02 <ross.beazley@bbc.co.uk>
2020-11-15 19:00:59 +0000

    Can instantiate dependency with simple string constructor

--- ioc-core/src/test/java/uk/co/rossbeazley/ioc_core/CanInjectIntoInstancesUsingReflection.kt
+++ ioc-core/src/test/java/uk/co/rossbeazley/ioc_core/CanInjectIntoInstancesUsingReflection.kt
@@ -60,7 +60,24 @@ class ReflectionIoCContainer : IoCContainer {
 
         known.forEach { it.set(into,things[it.type]) }
         unkown.forEach {
-            it.set(into,it.type.newInstance())
+            val newInstance = createInstanceForField(ofType = it.type)
+            it.set(into, newInstance)
+        }
+    }
+
+    private fun createInstanceForField(ofType: Class<*>): Any? {
+        val declaredConstructors = ofType.declaredConstructors
+
+        return when(declaredConstructors.size) {
+            0 -> ofType.newInstance()
+            1 -> {
+                val constructor = declaredConstructors[0]
+                val args = constructor.parameterTypes
+                    .map {it.newInstance()}
+                    .toTypedArray()
+                constructor.newInstance(*args)
+            }
+            else -> null
         }
     }
 }
